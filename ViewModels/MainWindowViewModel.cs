8e5eaa7f6216990e36186d16b4402d947a66d8d5
mohamed amdouni <midouamdouni4@gmail.com>
2026-06-15 20:52:45 +0000

    switched to avalonia

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace live_message_app.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace live_message_app.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
